b814c7e791460a6493697d84d6876c14f976a965
harshdubey1198 <amanad08052@gmail.com>
2023-07-20 08:41:25 +0530

    updated

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,5 +1,5 @@
 
-//code for fetching data from github using githubcalendar ..
+//code for fetching data from github using githubcalendar .. goal
 import React from "react";
 import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
