6e49291c12a98cd3146ff4049ccfb2e6facb1f14
harshdubey1198 <amanad08052@gmail.com>
2023-07-21 21:54:09 +0530

    .

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -4,9 +4,9 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 
-import Particle from '../Particle3';
+// import Particle from '../Particle3';
 import pdf from "../Harsh_it_29.pdf";
-
+import "./resume.css";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer";
 import "react-pdf/dist/esm/Page/TextLayer";
@@ -26,27 +26,28 @@ const Resume = () => {
   return (
     <div>
       <Container fluid className="resume-section" style={{paddingTop:"131px"}}>
-        <Particle />
+        
        <Row style={{color:"white"}}>
         <h1>RESUME </h1><h6>PREVIEW</h6>
        </Row>
 
         <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={1} scale={width > 786 ? 1.0 : 0.6} />
           </Document>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px",marginBottom:"100px" }}
-          >
-            <AiOutlineDownload />
-               Download Resume
-          </Button>
+        <Button
+  variant="primary"
+  href={pdf}
+  target="_blank"
+  style={{ maxWidth: "250px", marginBottom: "100px" }}
+>
+  <AiOutlineDownload />
+  Download Resume
+</Button>
+
         </Row>
       </Container>
     </div>
