26dedfa7cf63e3f4cd1b7ccf8d962bf2a4a2654c
HARSH DUBEY <amanad08052@gmail.com>
2023-09-22 09:45:02 +0530

    /

--- src/components/Projects/project.js
+++ src/components/Projects/project.js
@@ -62,7 +62,8 @@ function Projects() {
           My Recent <strong className="purple">Works</strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Here are a few projects I've worked on recently. 
+          {/* hello */}
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "100px" }}>
           {projects.map((project, index) => (
