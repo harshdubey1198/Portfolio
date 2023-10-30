0e99f3b26caa2dd18f1ee02dc6f790a990be0a4d
HARSH DUBEY <amanad08052@gmail.com>
2023-10-30 19:01:53 +0530

    major changes in /resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,11 +1,9 @@
-//Resume page for Portfolio.
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
-
- import Particle from '../Particle4';
-import pdf from "./Harsh_it_29.pdf";
+import Particle from '../Particle4';
+// import pdf from "./Harsh_it_29.pdf";
 import pdf2 from "./Harsh_IT_resume.pdf";
 import "./resume.css";
 import { Document, Page, pdfjs } from "react-pdf";
@@ -13,60 +11,37 @@ import "react-pdf/dist/esm/Page/AnnotationLayer";
 import "react-pdf/dist/esm/Page/TextLayer";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/harshdubey1198/portfolio/master/Harsh_it_29.pdf`
-
-
 const Resume = () => {
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
-    
     setWidth(window.innerWidth);
   }, []);
 
+  const isMobile = width <= 786;
+
   return (
-    <div>      <Particle/>
-
-      <Container fluid className="resume-section" style={{paddingTop:"131px"}}>
-        
-       <Row style={{color:"white"}}>
-        <h1>RESUME </h1><h6>PREVIEW</h6>
-       </Row>
-       <Row style={{ justifyContent: "center", position: "relative" }}>
-        <Button
-  variant="primary"
-  href={pdf}
-  target="_blank"
-  style={{ maxWidth: "250px", marginBottom: "100px" }}
->
-  <AiOutlineDownload />
-  Download Resume template 1
-</Button>
-
+    <div>
+      <Particle />
+      <Container fluid className="resume-section" style={{ paddingTop: "131px" }}>
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf2}
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            Download Resume
+          </Button>
         </Row>
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.0 : 0.6} />
+          <Document file={pdf2} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={isMobile ? 0.5 : 1.0} />
           </Document>
         </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-        <Button
-  variant="primary"
-  href={pdf2}
-  target="_blank"
-  style={{ maxWidth: "250px", marginBottom: "100px" }}
->
-  <AiOutlineDownload />
-  Download Resume template 2
-</Button>
-
-        </Row>
-        
       </Container>
     </div>
-  )
+  );
 }
 
-export default Resume
-//make sure to edit url link for resume
+export default Resume;
